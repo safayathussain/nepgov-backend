9484326c604e3a7e48e22a81275412ee105ca208
safayathussain <safayatofficial1@gmail.com>
2025-03-09 03:29:20 +0600

    token fix

--- src/utils/function.js
+++ src/utils/function.js
@@ -15,7 +15,7 @@ const generateAccessToken = (user) => {
     { id: user._id, role: user.role },
     process.env.JWT_ACCESS_SECRET,
     {
-      expiresIn: "10s",
+      expiresIn: "1h",
     }
   );
 };
